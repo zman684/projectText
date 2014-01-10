1ff82e922c3da695a9080e14c2649a2f0809f657
Zach Eriksen <toorealc@yahoo.com>
2014-01-09 20:37:37 -0600

    Reverts back to Text 1

---
+++ Game/src/engine/test.java
@@ -0,0 +1,11 @@
+package engine;
+
+public class test {
+	public static void main(String[] args) {
+		//Starts the game
+		Engine.start();
+		//Tutorial.readMap();
+		//Runs the game
+		Engine.menu();
+	}
+}
